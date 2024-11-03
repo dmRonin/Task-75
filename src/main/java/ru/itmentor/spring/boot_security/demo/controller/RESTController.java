03a95a5cd2cfb492f9be79682bba837e5e05209d
Dmitrii <dim.sent@icloud.com>
2024-11-03 17:16:01 +0700

    fix rest

--- src/main/java/ru/itmentor/spring/boot_security/demo/controller/RESTController.java
+++ src/main/java/ru/itmentor/spring/boot_security/demo/controller/RESTController.java
@@ -1,8 +1,15 @@
 package ru.itmentor.spring.boot_security.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.itmentor.spring.boot_security.demo.model.Role;
@@ -44,4 +51,26 @@ public class RESTController {
     public Role getRoleById(@PathVariable("id") int id) {
         return roleServiceImpl.findAllRoles().get(id);
     }
+
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        userService.createUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    @PatchMapping("/users/{id}")
+    public ResponseEntity<User> updateUser(@RequestBody User user, @PathVariable("id") int id) {
+        userService.master(id, user);
+        return ResponseEntity.ok(user);
+    }
+
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable("id") int id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
 }
+
